3d9dcc9d75eaac270558cf614665e6dcc3d0656f
dmratcliffe <sackboy11@gmail.com>
2019-04-03 18:10:19 -0500

    fetchInformation should be complete, needs implementation (and doublecheck the callback will work)

--- js/LightControl.js
+++ js/LightControl.js
@@ -19,21 +19,54 @@ class LightControl {
 
     //if not given an adress assume we are on
     //the esp. Otherwise use the adress given to us.
-    constructor(address){
-        if(address === undefined){
+    constructor(address) {
+        if (address === undefined) {
             this.address = location.hostname;
-        }else{
+        } else {
             this.address = address;
         }
-        this.address = "http://" + this.address + "/";
+        this.address = "http://" + this.address + ":888/";
     }
 
-    fetchInformation(){
-        $.get(this.address + "all", function(data){
-            this.jsonINFO = "{";
-            $.each(data, function(i, f){
-                console.log(f.name + " " + f.value);
+    //this fucntion allows for fethcing information
+    //from the esp.
+    //when done, it sends a callback of a json query with all relevant information
+    //passed as the variable
+    fetchInformation(callback) {
+        $.get(this.address + "all", function (data) {
+            this.jsonINFO = new Array();
+            var temp = new Array();
+            $.each(data, function (i, f) {
+                //are we looking at an actual value or a section...
+                //because apparently /all doesn't return html
+                //but actually returns an array of HTML specifications
+                if (f.value != undefined) {
+                    //info we care about
+                    var name = f.name, val = f.value, options = f.options, min=f.min, max = f.max;
+                    
+                    //plunk it into an array
+                    var itemProps = new Array();
+                    itemProps['value'] = val;
+
+                    //some that might not always be there...
+                    if(options !== undefined)
+                        itemProps['options'] = options;
+                    if(min !== undefined)
+                        itemProps['min'] = min;
+                    if(max !== undefined)
+                        itemProps['max'] = max;
+
+                    //setup the name (makes it easy to find later)
+                    temp[name] = itemProps;
+                }
             });
+            //setup the full
+            this.jsonINFO = temp;
+
+            //make sure theres a function to callback...
+            if(callback && {}.toString.call(callback) === '[object Function]'){
+                callback(this.jsonINFO);
+            }
         });
     }
 }
