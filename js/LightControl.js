68c9a0e9eab15d520ebbb5fe0fadc1fcb44c5b88
unknown <sackboy11@gmail.com>
2019-04-03 13:51:09 -0500

    Removed the websocket code, it actually didn't do anything. No response. Even the public code errors out...

--- js/LightControl.js
+++ js/LightControl.js
@@ -4,7 +4,7 @@
     
     Functions for each operation should be written and clear.
 
-    This is mostly a conversion from the "simple.js" file that
+    This is mostly a conversion from the "simple.js"/"app.js" file that
     was already being used. I just didn't like how it was tied 
     to elements which made it really hard to change anything
     without digging through the HTML.
@@ -26,28 +26,4 @@ class LightControl {
             this.address = address;
         }
     }
-
-    //Returns a json array with information
-    //about the settings saved on the ESP
-    //  onSuccess returns <JSON DATA>
-    //  onFailure returns <-1>
-    fetchEssentialStatus(){
-        var ws = new ReconnectingWebsocket('ws://' + this.address + ':81/', ['arduino']);
-
-        ws.onmessage = function(returned){
-            var initialData = returned.data;
-            if(initialData != null){
-                var jsonData = JSON.parse(initialData);
-
-                return jsonData;
-
-            }else{
-
-                return -1;
-
-            }
-        }
-
-    }
-
 }
