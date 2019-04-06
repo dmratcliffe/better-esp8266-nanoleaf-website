bb451e7082d3a59d5a02f79b08dc6232b188a027
unknown <sackboy11@gmail.com>
2019-04-06 12:06:33 -0500

    A linked list will probably use to display triangles (it seems most similar to the actual strip setup)

---
+++ data/js/Triangle.js
@@ -0,0 +1,12 @@
+class Triangle {
+    constructor(parent, position){
+        this.parent = parent
+    }
+
+    getParent(){
+        return this.parent;
+    }
+    setParent(parent){
+        this.parent = parent;
+    }
+}
